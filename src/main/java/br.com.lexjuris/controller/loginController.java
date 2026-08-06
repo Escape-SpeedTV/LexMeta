ae868e1168b7994cccf5f8627593afd6f9e74125
William Nakano <escapspeed1@gmail.com>
2026-08-06 12:37:01 -0300

    Concluido a parte do back-end responsável pelo login e pelo redirecionamento do usuário. Em relação ao front-end, já foi inciado a estilização em CSS e estou finalizando ainda.
    Signed-off-by: William Nakano <escapspeed1@gmail.com>

--- src/main/java/br.com.lexjuris/controller/loginController.java
+++ src/main/java/br.com.lexjuris/controller/loginController.java
@@ -1,8 +1,12 @@
 package br.com.lexjuris.controller;
 
 public class loginController {
-    public void fazerLogin(String email, String senha){
-        System.out.println("E-mail recebido: " + email);
-        System.out.println("Senha recebida: " + senha);
+    public boolean fazerLogin(String email, String senha){
+        String emailCorreto = "miku@gmail.com";
+        String senhaCorreta = "1234567890";
+        if(email.equals(emailCorreto) && senha.equals(senhaCorreta)){
+            return true;
+        }
+        return false;
     }
 }
